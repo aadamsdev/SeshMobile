a5ed95042ed37e58f403d0a75654f0730c9b6f28
Andrew Adams <aadamsdev@gmail.com>
2017-11-09 09:47:15 -0500

    Spacing and progres bar for images

--- app/src/main/java/com/aadamsdev/seshmobile/ProductGalleryAdapter.java
+++ app/src/main/java/com/aadamsdev/seshmobile/ProductGalleryAdapter.java
@@ -1,13 +1,20 @@
 package com.aadamsdev.seshmobile;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 
@@ -43,9 +50,23 @@ public class ProductGalleryAdapter extends RecyclerView.Adapter<ProductGalleryAd
 
         Product product = products.get(position);
         ImageView imageView = holder.productImage;
+        final ProgressBar progressBar = holder.progressBar;
 
         Glide.with(context)
                 .load(product.getImageUrl())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(imageView);
     }
 
@@ -57,6 +78,7 @@ public class ProductGalleryAdapter extends RecyclerView.Adapter<ProductGalleryAd
     class ProductGalleryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         @BindView(R.id.product_image) ImageView productImage;
+        @BindView(R.id.progress) ProgressBar progressBar;
 
         ProductGalleryViewHolder(View itemView) {
             super(itemView);
